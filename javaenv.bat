aa697d6833c34c0a9f46dcd54c5b5fcb5e99bb85
Marc-André Girard <marcandr.girard@gmail.com>
2016-07-21 14:18:35 -0400

    creating the batch file
    create the script to run the command

---
+++ javaenv.bat
@@ -0,0 +1,14 @@
+@ECHO OFF
+SET java_version=%1
+SET JAVA6="C:\Program Files\Java\jdk1.6.0_43"
+SET JAVA7="C:\Program Files\Java\jdk1.7.0_79"
+SET JAVA8="C:\Program Files\Java\jdk1.8.0_73"
+
+call :unquote JAVA_HOME %java_version%
+
+goto :EOF
+
+:unquote
+  set %1=%~2
+  goto :EOF
+  

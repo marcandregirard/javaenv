c663d33006b38966fcd1c3276f0c90558c7f3ed0
Marc-André Girard <marcandr.girard@gmail.com>
2016-08-03 16:10:08 -0400

    changes for java file

--- javaenv.bat
+++ javaenv.bat
@@ -3,10 +3,7 @@ if "%1"=="" goto empty
 if "%1"=="echo" goto output
 if "%1"=="e" goto output
 SET java_version=%1
-SET JAVA6="C:\Program Files\Java\jdk1.6.0_43"
-SET JAVA7="C:\Program Files\Java\jdk1.7.0_79"
-SET JAVA8="C:\Program Files\Java\jdk1.8.0_73"
-
+FOR /f "delims=" %%x IN (%~dp0java_versions.store) DO (set "%%~x")
 call :unquote JAVA_HOME %java_version%
 
 goto :EOF
